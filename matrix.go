5bb6d4b6e27586b155687c0b9587804b931c0a76
Gabe Fierro <gtfierro225@gmail.com>
2013-10-18 08:14:28 -0700

    move matrix code to matrix.go

--- matrix.go
+++ matrix.go
@@ -6,24 +6,13 @@ import (
     "fmt"
 )
 
+
+/** Parallel **/
+
 var wg sync.WaitGroup
 
-func Generate_chan(lines []string) chan string{
-    in := make(chan string)
-    go func() {
-        defer close(in)
-        for _, line := range lines {
-            in <- line
-        }
-    }()
-    return in
-}
-
-
-func PCreateMatrix(in <-chan string) {
-    patent_index := 0
-    for line := range in {
-        patent_index += 1
+func PCreateMatrix(lines []string) {
+    for patent_index, line := range lines {
         wg.Add(1)
         go pemit_sparse(patent_index, line)
     }
@@ -32,6 +21,25 @@ func PCreateMatrix(in <-chan string) {
 
 func pemit_sparse(patent_index int, line string) {
     defer wg.Done()
+    tmpmap := make(map[string]int)
+    for _, token := range tokenize(line, false) {
+        tmpmap[token] += 1
+    }
+    for token, count := range(tmpmap) {
+        entry := "(" + strconv.Itoa(patent_index) + "," + strconv.Itoa(Dict[token]) + "," + strconv.Itoa(count) + ")"
+        fmt.Println(entry)
+    }
+}
+
+/** Sequential **/
+
+func CreateMatrix(lines []string) {
+    for patent_index, line := range lines {
+        emit_sparse(patent_index, line)
+    }
+}
+
+func emit_sparse(patent_index int, line string) {
     tmpmap := make(map[string]int)
     for _, token := range tokenize(line, false) {
         tmpmap[token] += 1
--- tokenizer.go
+++ tokenizer.go
@@ -89,20 +89,3 @@ func CreateDict(lines []string) {
     }
     fmt.Println("Finished creating token dictionary with", len(Dict), "items")
 }
-
-func CreateMatrix(lines []string) {
-    for patent_index, line := range lines {
-        emit_sparse(patent_index, line)
-    }
-}
-
-func emit_sparse(patent_index int, line string) {
-    tmpmap := make(map[string]int)
-    for _, token := range tokenize(line, false) {
-        tmpmap[token] += 1
-    }
-    for token, count := range(tmpmap) {
-        entry := "(" + strconv.Itoa(patent_index) + "," + strconv.Itoa(Dict[token]) + "," + strconv.Itoa(count) + ")"
-        fmt.Println(entry)
-    }
-}
