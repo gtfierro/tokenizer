607315ef95a9dd2f022f0d781e66593f99f109d5
Gabe Fierro <gtfierro225@gmail.com>
2013-10-29 11:14:46 -0700

    go fmt on dict.go

--- dict.go
+++ dict.go
@@ -44,12 +44,12 @@ func process() {
 each instance of `current` with `replacement`
 */
 func replace(target []byte, current, replacement byte) []byte {
-    for i, b := range target {
-        if b == current {
-            target[i] = replacement
-        }
-    }
-    return target
+	for i, b := range target {
+		if b == current {
+			target[i] = replacement
+		}
+	}
+	return target
 }
 
 /* loops through `target` and removes all
@@ -57,20 +57,20 @@ instances of `current`
 */
 
 func remove(target []byte, current byte) []byte {
-    i1 := 0
-    i2 := 0
-    length := len(target)
-    for {
-        if target[i2] != current {
-            target[i1] = target[i2]
-            i1 += 1
-        }
-        i2 += 1
-        if i2 == length {
-            break
-        }
-    }
-    return target[:i1]
+	i1 := 0
+	i2 := 0
+	length := len(target)
+	for {
+		if target[i2] != current {
+			target[i1] = target[i2]
+			i1 += 1
+		}
+		i2 += 1
+		if i2 == length {
+			break
+		}
+	}
+	return target[:i1]
 }
 
 func deliver(line []byte) {
@@ -124,15 +124,15 @@ func CreateDict(filename string) {
 	go readFile(filename, fileChannel)
 	go outputDict()
 	fmt.Println("Creating token dictionary")
-    for i := 0; i < 100; i++ {
-        tokenwg.Add(1)
-        go func() {
-            for line := range fileChannel {
-                deliver(line)
-            }
-            tokenwg.Done()
-        }()
-    }
+	for i := 0; i < 100; i++ {
+		tokenwg.Add(1)
+		go func() {
+			for line := range fileChannel {
+				deliver(line)
+			}
+			tokenwg.Done()
+		}()
+	}
 	tokenwg.Wait()
 	doneChannel <- true
 	fmt.Println("Finished creating token dictionary with", len(Dict), "items")
